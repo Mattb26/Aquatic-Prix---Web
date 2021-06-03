d0d2c833d469226fb5ddaf251ec13843daa85146
bostero854 <bostero854@gmail.com>
2021-06-03 00:08:21 -0300

    Conexion a la base de datos

---
+++ AquaticApi/DataAccess/Conexion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AquaticApi.DataAccess
+{
+    public class Conexion
+    {
+        public static string StringConexion()
+        {
+
+
+            try
+            {
+
+                string Conexion = "Server=" + Environment.GetEnvironmentVariable("SQL_SERVICE") +
+                                       "; Database=" + Environment.GetEnvironmentVariable("SQL_DATABASE") +
+                                       "; User id=" + Environment.GetEnvironmentVariable("USERNAME_SQL") +
+                                       "; Password=" + Environment.GetEnvironmentVariable("PASSWORD_SQL") + ";";
+
+                return Conexion;
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ AquaticApi/DataAccess/Contacto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AquaticApi.DataAccess
+{
+    public class Contacto
+    {
+        public bool Agregar(Models.Contacto contacto)
+        {
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(Conexion.StringConexion()))
+                {
+                    cn.Open();
+                    using (SqlTransaction transaction = cn.BeginTransaction())
+                    {
+                        using (SqlCommand cmd = new SqlCommand("sp_a_contacto", cn, transaction))
+                        {
+                            cmd.CommandType = CommandType.StoredProcedure;
+
+                            try
+                            {
+
+                                cmd.Parameters.Add(new SqlParameter("@nombre", contacto.Nombre));
+                                cmd.Parameters.Add(new SqlParameter("@asunto", contacto.Asunto));
+                                cmd.Parameters.Add(new SqlParameter("@correoElectronico", contacto.CorreoElectronico));
+                                cmd.Parameters.Add(new SqlParameter("@mensaje ", contacto.Mensaje));
+
+                                cmd.ExecuteNonQuery();
+
+
+                                transaction.Commit();
+                                return true;
+                            }
+                            catch (SqlException)
+                            {
+                                transaction.Rollback();
+                                throw;
+                            }
+                            catch (Exception)
+                            {
+                                transaction.Rollback();
+                                throw;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
