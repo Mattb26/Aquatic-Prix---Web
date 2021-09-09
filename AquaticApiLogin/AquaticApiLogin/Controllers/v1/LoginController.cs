6464ec0e0ece512f9a25d9ef302b67d40b934dc4
bostero854 <bostero854@gmail.com>
2021-09-08 23:22:15 -0300

    login

--- AquaticApiLogin/AquaticApiLogin/Controllers/v1/LoginController.cs
+++ AquaticApiLogin/AquaticApiLogin/Controllers/v1/LoginController.cs
@@ -1,10 +1,6 @@
 ﻿using AquaticApiLogin.Servicios;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AquaticApiLogin.Controllers.v1
 {
