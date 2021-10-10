c327f2483fd04aa376eb7518a9a4811703f4a731
bostero854 <bostero854@gmail.com>
2021-10-10 00:16:51 -0300

    Estadisticas

---
+++ AquaticAPIEstadistica/AquaticAPIEstadistica/Controllers/EstadisticaController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AquaticAPIEstadistica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstadisticaController : ControllerBase
+    {
+        [HttpGet("{idUsuario}/estadisticas")]
+        public IActionResult Get([Required] Int32 idUsuario)
+        {
+            //Deal.Estadistica estadistica;
+            try
+            {
+                //estadistica = new Deal.Estadistica();
+
+                return Ok();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpGet("estadisticas")]
+        public IActionResult Get()
+        {
+            //Deal.Estadistica estadistica;
+            try
+            {
+                //estadistica = new Deal.Estadistica();
+
+                return Ok();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
